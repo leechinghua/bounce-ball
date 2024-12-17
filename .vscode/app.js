d28f09379e0ab2aeafaa0a18292ae5ca75b2e8c4
leechinghua <leechenghua107@gmail.com>
2024-12-17 11:36:03 +0800

    基本設定

---
+++ .vscode/app.js
@@ -0,0 +1,10 @@
+const c = document.getElementById("myCanvas");
+const canvasHeight = c.height;
+const canvasWidth = c.width;
+var ctx = c.getContext("2d");
+
+function draw() {
+  console.log("畫圖");
+}
+
+// let game = setInterval(draw, 25);

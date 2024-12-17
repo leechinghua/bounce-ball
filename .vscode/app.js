254d5330d778b1501b66e07a311cac02f4cfcd97
leechinghua <leechenghua107@gmail.com>
2024-12-17 13:13:42 +0800

    讓球在範圍內彈跳

--- .vscode/app.js
+++ .vscode/app.js
@@ -5,8 +5,29 @@ var ctx = c.getContext("2d");
 let circle_x = 160;
 let circle_y = 60;
 let radius = 20;
+let xSpeed = 20;
+let ySpeed = 20;
 
 function draw() {
+  // 更動球的座標
+  // 確認右邊界
+  if (circle_x >= canvasWidth - radius) {
+    xSpeed *= -1;
+  }
+  // 確認左邊界
+  if (circle_x <= radius) {
+    xSpeed *= -1;
+  }
+  // 確認上邊界
+  if (circle_y <= radius) {
+    ySpeed *= -1;
+  }
+  // 確認下邊界
+  if (circle_y >= canvasHeight - radius) {
+    ySpeed *= -1;
+  }
+  circle_x += xSpeed;
+  circle_y += ySpeed;
   //   畫出黑色背景
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvasWidth, canvasHeight);

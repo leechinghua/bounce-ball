7edaa1d2a5bf908aaf26cca6a8e1b2d1ad2537a2
leechinghua <leechenghua107@gmail.com>
2024-12-17 12:20:00 +0800

    畫出黑色背景/畫出圓球

--- .vscode/app.js
+++ .vscode/app.js
@@ -2,9 +2,20 @@ const c = document.getElementById("myCanvas");
 const canvasHeight = c.height;
 const canvasWidth = c.width;
 var ctx = c.getContext("2d");
+let circle_x = 160;
+let circle_y = 60;
+let radius = 20;
 
 function draw() {
-  console.log("畫圖");
+  //   畫出黑色背景
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+  //   畫出圓球
+  ctx.beginPath();
+  ctx.arc(circle_x, circle_y, radius, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.fillStyle = "yellow";
+  ctx.fill();
 }
 
-// let game = setInterval(draw, 25);
+let game = setInterval(draw, 25);
